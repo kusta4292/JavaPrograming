49cb5e1686d61610d1bbb002c95997388919457d
Kamil USTA <kusta@gmail.com>
2022-12-17 23:45:03 +0100

    car class changes executed

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,16 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry",2020,20000,"Gray",23566);
+
+        Tesla tesla = new Tesla("Model S", 2022, 94990, "Red", 0);
+
+        BMW bmw = new BMW("X5", 2020,75000, "Black",1200);
+    }
+
+
+
+}
