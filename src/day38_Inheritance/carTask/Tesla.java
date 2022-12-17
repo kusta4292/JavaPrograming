4c12afa9427d66473e90cd0ba2f663a4149cacd7
Kamil USTA <kusta@gmail.com>
2022-12-17 23:15:07 +0100

    car class changes executed

--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -1,7 +1,13 @@
 package day38_Inheritance.carTask;
 
 public class Tesla extends Car{
-    public Tesla(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
     }
+
+    public void autopilot(){
+
+        System.out.println(brand+""+model+"in autopilot mode");
+    }
+
 }
