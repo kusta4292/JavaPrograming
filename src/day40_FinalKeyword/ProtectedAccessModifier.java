201aae47419317fba593ac9c73c0fea0529fa3fb
Kamil USTA <kusta@gmail.com>
2023-03-31 13:29:02 +0200

    ProtectedAccessModifier And Final Keys

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,20 @@
+package day40_FinalKeyword;
+
+import utilities.AccessModifiers_Test4;
+
+public class ProtectedAccessModifier {
+
+    static String name1 ="Cydeo"; //default access modifier
+    protected static String name2 = "WoodenSpoon";//protected
+
+
+    static void method1(){
+
+
+    }
+
+protected static void method2(){
+
+
+}
+}
