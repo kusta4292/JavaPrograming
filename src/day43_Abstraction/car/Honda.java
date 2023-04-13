d28fdf4434a5ead1745dd488d4e42879d5ad15e9
Kamil USTA <kusta@gmail.com>
2023-04-13 04:11:52 +0200

    Abstract addition

--- src/day43_Abstraction/car/Honda.java
+++ src/day43_Abstraction/car/Honda.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.car;
 
-public final class  Honda extends Car {
+public  class  Honda extends Car {
 
 
     public Honda(String model, String color, int year, double price) {
